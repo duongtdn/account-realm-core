66669fb690a5827b7e74532528add2543e041654
duongtdn <duongtdn@gmail.com>
2019-01-07 08:50:49 +0000

    add webpack build for client

---
+++ webpack.build.config.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const path = require("path");
+
+module.exports = {
+  entry: {
+    signup: ['./clients/scripts/sign-up.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, "build/clients"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js?$)|(\.jsx?$)/,
+        use: 'babel-loader',    
+      }
+    ]
+  },
+  mode: 'development',
+  devtool: 'inline-source-map'
+}
