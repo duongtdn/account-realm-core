d3ea17df68c34ed7f72f6200b47ac6d926696a0c
duongtdn <duongtdn@gmail.com>
2019-01-08 08:32:02 +0700

    link font-awesome

--- src/clients/templates/html.js
+++ src/clients/templates/html.js
@@ -9,7 +9,7 @@ module.exports = ({title, script, dom}) => `
     <meta http-equiv="x-ua-compatible" content="ie=edge">  
     <title>${title}</title>         
     <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
-	  <!-- <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css"> -->
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
 
   <body>    
