11930af59a4ef8a15f4ee3a7795df5486ee18d75
duongtdn <duongtdn@gmail.com>
2019-01-08 19:40:25 +0700

    serve built client script

--- tests/server.js
+++ tests/server.js
@@ -1,9 +1,13 @@
 "use strict"
 
+const path = require('path')
 const express = require('express')
 const api = require('../src/api/main')
 
 const app = express()
+
+app.use('/assets', express.static(path.join(__dirname, '../build/clients')))
+
 app.use('/', api.generate())
 
 app.listen(3100, function() {
