28e3ff7c4ef6d577f3336fea25636f8ef764ccdb
duongtdn <duongtdn@gmail.com>
2019-01-09 07:47:24 +0000

    include target origin in rendered html

--- src/api/sign-up-form.js
+++ src/api/sign-up-form.js
@@ -8,9 +8,10 @@ const SignUp = require('../../build/components/SignUp')
 
 function render(helpers) {
   return function(req, res) {
+    const data = { targetOrigin: 'http://localhost:3200'}
     const reactDom = renderToString(React.createElement(SignUp.default))
     res.writeHead( 200, { "Content-Type": "text/html" } );
-    res.end(html({title: 'Signup', dom: reactDom, script: `${process.env.CDN}/signup.js`}))
+    res.end(html({title: 'Signup', data, dom: reactDom, script: `${process.env.CDN}/signup.js`}))
   }
 }
 
