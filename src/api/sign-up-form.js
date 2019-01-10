1a5418b1916b371511a3ffcceab2130a415c85b8
duongtdn <duongtdn@gmail.com>
2019-01-10 23:08:39 +0700

    handle error return from server

--- src/api/sign-up-form.js
+++ src/api/sign-up-form.js
@@ -5,7 +5,7 @@ const html = require('../clients/templates/html')
 function render(helpers) {
   return function(req, res) {
     if (!(req.query && req.query.realm && req.query.app)) {
-      _renderError(res, 403, 'Forbidden')
+      _renderError(res, '403 Forbidden', 'Application is not registered')
       return
     }
     const realm = req.query.realm
@@ -14,15 +14,16 @@ function render(helpers) {
       if (apps && apps[0]) {
         _renderSignup(res, apps[0])
       } else {
-        _renderError(res, 403, 'Forbidden')
+        _renderError(res, '403 Forbidden', 'Application is not registered')
       }
     })
   }
 }
 
 function _renderError(res, code, detail) {
+  const data = { error: {code, detail} }
   res.writeHead( 403, { "Content-Type": "text/html" } );
-  res.end(html({title: 'Error'}))
+  res.end(html({title: 'Error', data, script: `${process.env.CDN}/error.js`}))
 }
 
 function _renderSignup(res, app) {
