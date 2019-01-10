1cd13a20e30856740f0a8e1bd8f76ad1214cc543
duongtdn <duongtdn@gmail.com>
2019-01-10 23:06:30 +0700

    handle error, postMessage return a promise

--- src/clients/scripts/message.js
+++ src/clients/scripts/message.js
@@ -1,26 +1,45 @@
 "use strict"
 
 export function postMessage(code) {
-  if (!window.parent) {
-    return
-  }
-
-  const target = window.parent  
-  const msg = { code: code }
-  if (code === 'iframe.done') {
-    msg.status = status
-    const obj = JSON.parse(message)
-    for (let k in obj) {
-      msg[k] = obj[k]
+  return new Promise( (resolve, reject) => {
+    if (!window.parent) {
+      reject()
+      return
     }
-    if (status == 200 || status == 404) {
-      target.postMessage(msg, window.__data.targetOrigin)
-    } else if (status == 403) {
-      target.postMessage(msg, "*")
-      throw new Error('403 Forbidden')
+    if (__data.error) {
+      reject({..._data.error})
+      return
     }
-  } else {
-    target.postMessage(msg, window.__data.targetOrigin)
-  }
-
+    if (!__data.targetOrigin) {
+      reject({code: '403 Fobidden', detail: ''})
+      return
+    }
+    if (!document.referrer) {
+      reject({code: '403 Fobidden', detail: ''})
+      return
+    }
+    if (document.referrer.replace(/\/+$/, '') === window.__data.targetOrigin.replace(/\/+$/, '')) {
+      const target = window.parent  
+      const msg = { code: code }
+      if (code === 'iframe.done') {
+        msg.status = status
+        const obj = JSON.parse(message)
+        for (let k in obj) {
+          msg[k] = obj[k]
+        }
+        if (status == 200 || status == 404) {
+          target.postMessage(msg, window.__data.targetOrigin)
+        } else if (status == 403) {
+          target.postMessage(msg, "*")
+          throw new Error('403 Forbidden')
+        }
+      } else {
+        target.postMessage(msg, window.__data.targetOrigin)
+      }  
+      resolve()
+    } else {
+      reject({code: '403 Fobidden', detail: 'Cross Origin Blocked'})
+    }
+  })
+  
 }
