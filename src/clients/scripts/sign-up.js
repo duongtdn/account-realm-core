5979827c4792bd16ad2cf7e53bdcc1ce4789a2a7
duongtdn <duongtdn@gmail.com>
2019-01-10 23:07:19 +0700

    render SignUp or Error depends on postMessage promise

--- src/clients/scripts/sign-up.js
+++ src/clients/scripts/sign-up.js
@@ -1,25 +1,18 @@
 "use strict"
 
 import React from 'react'
-import { hydrate, render } from "react-dom"
+import { render } from "react-dom"
 
 import { postMessage } from "./message"
 import SignUp from '../templates/components/SignUp'
+import Error from '../templates/components/Error'
 
 document.addEventListener("DOMContentLoaded", function(event) {
   console.log('# Form: document loaded')
   postMessage('iframe.loaded')
+    .then( _ => render(<SignUp close = {xclose} />, document.getElementById("root")))
+    .catch( err => render(<Error code = {err.code} detail = {err.detail} />, document.getElementById("root")))
 });
-
-window.addEventListener("message", (event) => {
-  if (event.origin !== __data.targetOrigin) {
-    return
-  }
-  if (event.data === 'iframe.ack') {
-    render(<SignUp close = {xclose} />, document.getElementById("root"))
-  }
-})
-
 
 function xclose() {
   postMessage('iframe.close')
