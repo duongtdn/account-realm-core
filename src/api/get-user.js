a65114cf94b29e2b3c2502e71aedbae747c8c875
duongtdn <duongtdn@gmail.com>
2019-01-11 07:21:08 +0000

    update implementation code

--- src/api/get-user.js
+++ src/api/get-user.js
@@ -1,8 +1,16 @@
 "use strict"
 
-function done() {
+function done(helpers) {
   return function(req, res) {
-    res.status(404).send("email not found")
+    const username = req.query.u
+    helpers.Collections.Users.find({ username }, (users) => {
+      if (users && users[0]) {
+        /* todo: serialize user according role of requester. e.g. admin can see more information whay anonymous only see email */ 
+        res.status(200).json({ user: username })
+      } else {
+        res.status(404).json({ user: null })
+      }
+    })    
   }
 }
 
