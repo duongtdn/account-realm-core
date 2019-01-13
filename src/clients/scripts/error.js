3d59c9ee41f8bc2581d91f84fad0c052e49b4549
duongtdn <duongtdn@gmail.com>
2019-01-13 22:17:57 +0700

    error also post message indicate iframe close and done

--- src/clients/scripts/error.js
+++ src/clients/scripts/error.js
@@ -3,8 +3,13 @@
 import React from 'react'
 import { render } from "react-dom"
 
-
+import { postMessage } from "./message"
 import Error from '../templates/components/Error'
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  postMessage('iframe.loaded').catch((err) => console.error(err))
+  postMessage('iframe.done', { status: window.__data.error.code })
+})
 
 render(<Error code = {window.__data.error.code} detail = {window.__data.error.detail} />, document.getElementById("root"))
 
