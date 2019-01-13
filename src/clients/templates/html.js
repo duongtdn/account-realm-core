9e68d53e8160f9d52c3774c849b85ea711f40782
duongtdn <duongtdn@gmail.com>
2019-01-13 22:16:17 +0700

    add argument style

--- src/clients/templates/html.js
+++ src/clients/templates/html.js
@@ -1,15 +1,17 @@
 "use strict"
 
-module.exports = ({title, data, script}) => `
+module.exports = ({title, data, style, script}) => `
 <!DOCTYPE html>
 <html class="w3-light-grey">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1,  shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">  
-    <title>${title}</title>         
+    <title>${title}</title>  
+    ${style === false? '' : `
     <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
 	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    `}     
   </head>
 
   <body>    
