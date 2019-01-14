bc911c7979877559af3ca51da577c4bf33ac8377
duongtdn <duongtdn@gmail.com>
2019-01-14 07:58:07 +0000

    moved some common function into libs/util

--- src/api/new-user.js
+++ src/api/new-user.js
@@ -3,6 +3,8 @@
 const uuid = require('uuid/v1')
 const jwt = require('jsonwebtoken')
 
+const { generateAuthenToken, setHttpCookie, serializeUser } = require('./libs/util')
+
 function validateParameters() {
   return function(req, res, next) {
     const realm = req.body.realm
@@ -10,6 +12,7 @@ function validateParameters() {
       res.status(400).send('realm is missing')
       return
     }
+    req.realm = realm
     const user = req.body.user
     if (!user || !user.email) {
       res.status(400).send('bad user object')
@@ -69,23 +72,6 @@ function createUser(helpers) {
   }
 }
 
-function generateAuthenToken(helpers) {
-  return function(req, res, next) {
-    const user = req.user
-    const realm = req.body.realm
-    helpers.Collections.Realms.find({ realmId: realm }, (realms) => {
-      if (realms && realms[0]) {
-        const secret = realms[0].secret
-        req.authenToken = jwt.sign({ uid: user.uid }, secret)  
-        next()
-      } else {
-        console.error('Database Error: Cannot get realm from Realms collection')
-        res.status(500).json({error: 'Database error'})
-      }
-    })        
-  }
-}
-
 function sendEmail(helpers) {
   return function(req, res, next) {
     if (helpers.sendEmail) {
@@ -105,31 +91,10 @@ function sendEmail(helpers) {
   }
 }
 
-function setHttpCookie() {
-  return function(req, res, next) {    
-    const user = req.user
-    const uid = jwt.sign(
-      {uid: user.uid}, 
-      process.env.AUTH_KEY_COOKIE_ENCODE || 'AUTH_KEY_COOKIE_ENCODE'
-    );    
-    const clientId = Math.random().toString(36).substr(2,9)
-    const session = { uid, clientId }
-    const cookie = JSON.stringify(session)
-    res.cookie('session', cookie, { httpOnly: true })
-    next()
-  }
-}
-
 function responseSuccess() {
   return function(req, res) {
     res.status(200).json({ user: serializeUser(req.user), token: req.authenToken })
   }
-}
-
-function serializeUser(user) {
-  delete user.uid
-  delete user.credentials
-  return user
 }
 
 module.exports = [validateParameters, checkUserExistance, createUser, generateAuthenToken, sendEmail, setHttpCookie, responseSuccess]
