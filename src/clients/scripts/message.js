970ad9db870ccec237d7a5c3cd6085bf73863aea
duongtdn <duongtdn@gmail.com>
2019-01-15 09:36:55 +0000

    not check document.referrer with origin anymore

--- src/clients/scripts/message.js
+++ src/clients/scripts/message.js
@@ -1,40 +1,20 @@
 "use strict"
 
 export function postMessage(code, data) {
-  return new Promise( (resolve, reject) => {
-    if (!window.parent) {
-      reject()
-      return
+  const target = window.parent  
+  const msg = { code: code }
+  for (let prop in data) {
+    msg[prop] = data[prop]
+  }
+  if (code === 'iframe.done') {    
+    const status = data.status
+    if (status == 200 || status == 404) {
+      target.postMessage(msg, window.__data.targetOrigin)
+    } else if (status == 403) {
+      target.postMessage(msg, "*")
+      throw new Error('403 Forbidden')
     }
-    if (!__data.targetOrigin) {
-      reject({code: '403 Fobidden', detail: ''})
-      return
-    }
-    if (!document.referrer) {
-      reject({code: '403 Fobidden', detail: ''})
-      return
-    }
-    if (document.referrer.replace(/\/+$/, '') === window.__data.targetOrigin.replace(/\/+$/, '')) {
-      const target = window.parent  
-      const msg = { code: code }
-      if (code === 'iframe.done') {
-        for (let prop in data) {
-          msg[prop] = data[prop]
-        }
-        const status = data.status
-        if (status == 200 || status == 404) {
-          target.postMessage(msg, window.__data.targetOrigin)
-        } else if (status == 403) {
-          target.postMessage(msg, "*")
-          throw new Error('403 Forbidden')
-        }
-      } else {
-        target.postMessage(msg, window.__data.targetOrigin)
-      }  
-      resolve()
-    } else {
-      reject({code: '403 Fobidden', detail: 'Cross Origin Blocked'})
-    }
-  })
-  
+  } else {
+    target.postMessage(msg, window.__data.targetOrigin)
+  }
 }
