3387157205dedcacb203e0db8e8facf4c57c28a2
duongtdn <duongtdn@gmail.com>
2019-01-15 09:38:04 +0000

    send height along with loaded event

--- src/clients/scripts/sign-up.js
+++ src/clients/scripts/sign-up.js
@@ -5,12 +5,10 @@ import { render } from "react-dom"
 
 import { postMessage } from "./message"
 import SignUp from '../templates/components/SignUp'
-import Error from '../templates/components/Error'
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  postMessage('iframe.loaded')
-    .then( _ => render(<SignUp close = {xclose} onSuccess={onSignupSuccess} />, document.getElementById("root")))
-    .catch( err => render(<Error code = {err.code} detail = {err.detail} />, document.getElementById("root")))
+  postMessage('iframe.loaded',  {height: 615})
+  render(<SignUp close = {xclose} onSuccess={onSignupSuccess} />, document.getElementById("root"))
 });
 
 function xclose() {
