47ca0a2be13d7e12bce4504f7243081035a1d0c7
duongtdn <duongtdn@gmail.com>
2019-01-24 07:54:13 +0000

    add TabProfile

--- src/clients/templates/components/MyAccount.js
+++ src/clients/templates/components/MyAccount.js
@@ -90,13 +90,233 @@ class TabPassword extends Component {
 class TabProfile extends Component {
   constructor(props) {
     super(props)
+    this.state = { error: {} }
+    this.originProfile = {
+      fullName: '',
+      displayName: '',
+      gender: '',
+      email:[''],
+      address: '',
+      phone: [''],
+      birthday: '',
+      picture: ''
+    }
+    this.updateProfile = this.updateProfile.bind(this)
+    this.resetState = this.resetState.bind(this)
+  }
+  componentWillMount() {
+    this._updateStateFromProps(this.props.user.profile)
+  } 
+  componentWillReceiveProps(props) {
+    this._updateStateFromProps(props.user.profile)
   }
   render() {
     return(
       <div className = "">
         <h4> Profile </h4> 
+        {/* Full Name */}
+        <p > 
+          <span >
+            <label>Full Name</label>
+            <label className="w3-right w3-text-red"> {this.state.error.fullName} </label> 
+            <input className={`w3-input w3-border ${this.state.error.fullName? 'w3-border-red': ''}`}
+                    type="text"
+                    value={this.state.fullName}
+                    onChange={this.getTyped('fullName')}
+            />
+          </span>          
+        </p>
+        {/* Display Name */}
+        <p>
+          <span className="w3-mobile" style={{width: '100%', display: 'inline-block'}}>
+            <label>Display Name</label>
+            <label className="w3-right w3-text-red"> {this.state.error.displayName} </label>
+            <input  className={`w3-input w3-border ${this.state.error.displayName? 'w3-border-red': ''}`}
+                    type="text"
+                    value={this.state.displayName}
+                    onChange = {this.getTyped('displayName')}
+            />
+          </span>
+        </p>
+        {/* Gender */}
+        <p>
+          <label style={{marginRight: '4px'}}> Gender: </label>
+
+          <input className="w3-radio" style={{marginRight: '4px'}} type="radio" name="gender" value="male" 
+                 checked={this.state.gender === 'male'} 
+                 onChange = { () => this.setState({gender: 'male'}) }
+          />
+          <label>Male</label>
+
+          <label style={{marginRight: '8px'}} />
+
+          <input className="w3-radio" style={{marginRight: '4px'}} type="radio" name="gender" value="female" 
+                 checked={this.state.gender === 'female'}
+                 onChange = { () => this.setState({gender: 'female'}) }
+          />
+          <label>Female</label>
+        </p>
+        {/* Birthday */}
+        <p>
+          <span className="w3-mobile" style={{width: '35%', display: 'inline-block'}}>
+            <label>Birthday</label>
+            <input  className="w3-input w3-border"
+                    type="text"
+                    placeholder="dd/mm/yyyy"
+                    value={this.state.birthday}
+                    onChange = {this.getTyped('birthday')}
+            />
+          </span>
+        </p>
+        {/* Emails */}
+        <p>
+          <label>Email</label>
+          <label className="w3-right w3-text-red"> {this.state.error.email} </label>
+          {
+            this.state.email.map((email, index) => {
+              return (
+                <span  key = {index} style={{display: 'block', marginBottom: '4px'}}>
+                  <input  className = {`w3-input w3-border ${this.state.error.email? 'w3-border-red': ''}`}
+                          type = "text"
+                          value = {email}
+                          disabled = {index === 0}
+                          onChange = {this.getTyped('email', index)}
+                  />
+                  <label  className = "w3-text-blue" 
+                          style = {{cursor: 'pointer', display: (index === this.state.email.length - 1) ? 'inline' : 'none'}} 
+                          onClick = {this.addMoreBox('email')} > 
+                    + Add more email 
+                  </label>
+                </span>
+              )
+
+            })
+          }
+        </p>
+        {/* Phones */}
+        <p>
+          <label>Phone</label>
+          <label className="w3-right w3-text-red"> {this.state.error.phone} </label>
+          {
+            this.state.phone.map((phone, index) => {
+              return (
+                <span  key = {index} style={{display: 'block', marginBottom: '4px'}}>
+                  <input  className = {`w3-input w3-border ${this.state.error.phone? 'w3-border-red': ''}`}
+                          type = "text"
+                          value = {phone}
+                          onChange = {this.getTyped('phone', index)}
+                  />
+                  <label  className = "w3-text-blue" 
+                          style = {{cursor: 'pointer', display: (index === this.state.phone.length - 1) ? 'inline' : 'none'}} 
+                          onClick = {this.addMoreBox('phone')} > 
+                    + Add more phone number 
+                  </label>
+                </span>
+              )
+
+            })
+          }
+        </p>
+        {/* Address */}
+        <p>
+          <label>Address</label>
+          <input  className="w3-input w3-border"
+                  type="text"
+                  value={this.state.address}
+                  onChange = {this.getTyped('address')}
+          />
+        </p>
+
+        <hr />
+
+        <p>
+          <button className="w3-button w3-blue w3-hover-blue w3-hover-opacity" onClick={this.updateProfile} disabled={this._isStateMatchOrigin()} > Save </button>
+          <label style={{marginRight: '8px'}} />
+          <button className="w3-button" onClick={this.resetState} disabled={this._isStateMatchOrigin()} > Reset </button>
+        </p>
       </div>
     )
+  }
+  getTyped(target, index) {
+    if (index !== undefined) {
+      return (evt) => {
+        const state = {}
+        state[target] = [...this.state[target]]
+        state[target][index] = evt.target.value
+        this.setState(state)
+      }
+    } else {
+      return (evt) => {
+        const state = {}
+        state[target] = evt.target.value
+        this.setState(state)
+      }
+    }    
+  }
+  addMoreBox(box) {
+    return (evt) => {
+      const _state = {}
+      const _container = [...this.state[box]]
+      _container.push('');
+      _state[box] = _container;
+      this.setState({ ..._state })
+    }
+  }
+  updateProfile() {
+    const error = {}
+    const profile = {}
+    for (let key in this.originProfile) {
+      profile[key] = this.state[key]
+    }
+    profile.phone = this.state.phone.filter(phone => phone.length > 0)
+    profile.email = this.state.email.filter(email => email.length > 0)
+    /* validate user input */
+    if (profile.birthday.length === 0) {
+      profile.birthday = 'N/A'
+    }
+    if (profile.fullName.length === 0) {
+      error.fullName = 'Please provide your name'
+    }
+    if (profile.displayName.length === 0) {
+      error.displayName = 'Please provide a display name'
+    }
+    // if (profile.phone.length === 0) {
+    //   error.phone = 'Please provide at least one number'
+    // }
+    if (profile.email.length === 0) {
+      error.email = 'Please provide at least one email'
+    }
+    if (Object.keys(error).length > 0) {
+      this.setState({error})
+      return
+    }
+    this.setState({ syncing: true })
+    xhttp.put(`${this.props.urlBasePath}/me/profile`, { profile },  (status, response) => {
+      this.setState({ syncing: false })
+      if (status === 200) {
+        this.props.onSuccess && this.props.onSuccess(profile)
+      } else {
+        const error = `Error: ${status}`
+        this.props.onError && this.props.onError(error)
+      }
+    })
+  }
+  resetState() {
+    this._updateStateFromProps(this.props.user.profile)
+  }
+  _updateStateFromProps(profile) {   
+    this._getOriginProfile(profile).setState({...this.state, ...this.originProfile})
+  }
+  _getOriginProfile(profile) {
+    for (let key in this.originProfile) {
+      if (profile[key]) {
+        this.originProfile[key] = profile[key] === 'N/A' ? '' : profile[key]
+      }
+    }
+    return this
+  }
+  _isStateMatchOrigin() {
+    return Object.keys(this.originProfile).every( key => this.state[key] === this.originProfile[key])
   }
 }
 
@@ -171,7 +391,9 @@ class Tabs extends Component {
   renderTab(tab) {
     return React.createElement(tabs[`Tab${_titleCase(tab)}`], {
       urlBasePath : this.props.urlBasePath,
-      user: this.props.user
+      user: this.props.user,
+      onSuccess: this.props.onSuccess,
+      onError: this.props.onError
     })
   }  
 }
@@ -180,7 +402,7 @@ export default class MyAccount extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      tab: 'password'
+      tab: 'profile'
     }
     this.tabs = [
       { icon: 'fa-key', name: 'password', label: 'change password' }, 
@@ -199,6 +421,8 @@ export default class MyAccount extends Component {
               onSelectTab = { (tab) => this.setState({ tab }) }
               user = { this.props.user }
               urlBasePath = { this.props.urlBasePath }
+              onSuccess = { data => console.log(data) }
+              onError = { err => console.log(err) }
         />
       </div>
     )
--- tests/client.js
+++ tests/client.js
@@ -8,6 +8,13 @@ import SignIn from '../src/clients/templates/components/SignIn'
 import MyAccount from '../src/clients/templates/components/MyAccount'
 
 console.log('Client script')
+const user = {
+  username : 'awesome@team.com',
+  profile: {
+    fullName: 'Awesome Tester',    
+    email: ['awesome@team.com']
+  }
+}
 
 class App extends Component {
   constructor(props) {
@@ -17,7 +24,7 @@ class App extends Component {
     return (
       <div> 
         {/* <SignIn urlBasePath = '/api' close = {() => console.log('close')} /> */}
-        <MyAccount urlBasePath = '/api' user = {{username : 'awesome@team.com', profile: {email: ['awesome@team.com']} }} />
+        <MyAccount urlBasePath = '/api' user = {user} />
       </div>
     )
   }
