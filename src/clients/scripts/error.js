b3ae539759bea99af045259c36bc168d9f7c355e
duongtdn <duongtdn@gmail.com>
2019-01-27 16:08:50 +0700

    remove catch since postMessage is not return a promise anymore

--- src/clients/scripts/error.js
+++ src/clients/scripts/error.js
@@ -7,7 +7,7 @@ import { postMessage } from "./message"
 import Error from '../templates/components/Error'
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  postMessage('iframe.loaded').catch((err) => console.error(err))
+  postMessage('iframe.loaded')
   postMessage('iframe.done', { status: window.__data.error.code })
 })
 
